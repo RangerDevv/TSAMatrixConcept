7cc59f14531fcbec7d67820dfd435a02498be977
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2025-07-19 00:55:02 -0400

    appwrite stuff

---
+++ src/lib/ids.ts
@@ -0,0 +1,2 @@
+export const DB_ID = "";
+export const COLLECTION = {};
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,21 @@
-// place files you want to import through the `$lib` alias in this folder.
+import {
+  Client,
+  Account,
+  ID,
+  Databases,
+  Permission,
+  Role,
+  Teams,
+  Storage,
+} from "appwrite";
+
+export const client = new Client();
+
+const appwriteClient = client
+  .setEndpoint("https://nyc.cloud.appwrite.io/v1")
+  .setProject("687970000022421d1877"); // Replace with your project ID
+
+export const appwriteDatabases = new Databases(appwriteClient);
+export const appwriteStorage = new Storage(appwriteClient);
+export const appwriteUser = new Account(appwriteClient);
+export const appwriteTeams = new Teams(appwriteClient);
