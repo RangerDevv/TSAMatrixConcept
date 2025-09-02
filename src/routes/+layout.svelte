a092b55b4376e0052884b0bcf32593d6911ac50b
RangerDevv <66392962+RangerDevv@users.noreply.github.com>
2025-09-02 00:28:35 -0400

    login page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,5 +7,5 @@
     <slot />
 </main>
 <footer>
-    <p class="text-center">Made with ❤️ by RangerDevv</p>
+    <p class="text-center pt-12">Made with ❤️ by RangerDevv</p>
 </footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,12 +10,14 @@
     }
 </script>
 
-<form on:submit={handleSubmit}>
+<p class="text-center text-lg pt-7">TSA MATRIX</p>
+
+<form on:submit={handleSubmit} class="flex flex-col gap-4 p-4 max-w-md mx-auto mt-10 border border-gray-300 rounded-lg">
     <label for="email">Email:</label>
-    <input type="email" id="email" bind:value={email} required />
+    <input type="email" id="email" bind:value={email} required class="border border-gray-300 p-2 rounded-lg bg-gray-400" />
     <label for="name">Name:</label>
-    <input type="text" id="name" bind:value={name} required />
+    <input type="text" id="name" bind:value={name} required class="border border-gray-300 p-2 rounded-lg bg-gray-400" />
     <label for="password">Password:</label>
-    <input type="password" id="password" bind:value={password} required />
-    <button type="submit" on:click={handleSubmit}>Sign Up</button>
+    <input type="password" id="password" bind:value={password} required class="border border-gray-300 p-2 rounded-lg bg-gray-400" />
+    <button type="submit" on:click={handleSubmit} class="bg-[#658BFF] text-white p-2 rounded-lg">Sign Up</button>
 </form>
